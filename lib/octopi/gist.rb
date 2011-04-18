0417ac7d040fac9854ab9ec31aa302e90d873d21
Ken Erickson <bookworm.productions@gmail.com>
2011-04-18 09:00:38 -0700

    starting on gist support

--- lib/octopi/gist.rb
+++ lib/octopi/gist.rb
@@ -17,7 +17,35 @@ module Octopi
       result = get("#{base_uri}/#{id}")
       # This returns an array of Gists, rather than a single record.
       new(result["gists"].first)
-    end
+    end 
+
+    def self.new(files_list,private_gist=false)
+		  files = []  
+			files_list.each do |file|  
+				files.push({
+	        :input     => File.read(file),
+	        :filename  => file,
+	        :extension => (File.extname(file) if file.include?('.'))
+	      })
+			end 
+			write(files, private_gist) 
+		end     
+		
+		def self.write(files,private_gist=false)   
+			data     = data(files,private_gist)
+			response = Api.api.post('/new', data)
+		end
+		
+		def self.data(files,private_gist=false) 
+			data = {}
+	    files.each do |file|
+	      i = data.size + 1
+	      data["file_ext[gistfile#{i}]"]      = file[:extension] ? file[:extension] : '.txt'
+	      data["file_name[gistfile#{i}]"]     = file[:filename]
+	      data["file_contents[gistfile#{i}]"] = file[:input]
+	    end   
+	    data.merge(private_gist ? { 'action_button' => 'private' } : {}) 
+		end
     
     # def files
     #   gists_folder = File.join(ENV['HOME'], ".octopi", "gists")
